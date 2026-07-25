dff6b50436c0a56cf092514d04cc6f9253f62edf
kynex7510 <nikkitten3@gmail.com>
2026-07-25 08:20:15 +0200

    Executable allocator address override

--- Source/Allocator.c
+++ Source/Allocator.c
@@ -121,22 +121,48 @@ Result ctrlHeapFree(size_t pageIndex, size_t numPages) {
 }
 
 static size_t maxPageForCodeBase(size_t pageBase) {
+    const u32 CODE_START = 0x100000;
+    const u32 CODE_END = 0x4000000;
+    const u32 SPECIAL_CODE_START = 0x14000000;
+    const u32 SPECIAL_CODE_END = 0x1C000000;
+    const u32 VA_END = 0x40000000;
+
     const u32 addr = ctrlPageIndexToAddr(pageBase);
 
-    if (addr >= 0x100000 && addr <= 0x4000000)
-        return ctrlAddrToPageIndex(0x4000000);
+#if defined(CTRL_CFG_EXEC_SIZE)
+    u32 addrEnd = ctrlAlignUp(addr + CTRL_CFG_EXEC_SIZE, CTRL_PAGE_SIZE);
+    if (addrEnd < addr)
+        addrEnd = VA_END;
 
-    if (addr >= 0x14000000 && addr <= 0x1C000000)
-        return ctrlAddrToPageIndex(0x1C000000);
+    // Check known ranges.
+    if (addr >= CODE_START && addr <= CODE_END) {
+        if (addrEnd > CODE_END)
+            svcBreak(USERBREAK_PANIC);
+    } else if (addr >= SPECIAL_CODE_START && addr <= SPECIAL_CODE_END) {
+        if (addrEnd > SPECIAL_CODE_END)
+            svcBreak(USERBREAK_PANIC);
+    }
 
-    // Shouldn't happen.
-    svcBreak(USERBREAK_PANIC);
-    return 0;
+    return ctrlAddrToPageIndex(addrEnd);
+#else
+    if (addr >= CODE_START && addr <= CODE_END)
+        return ctrlAddrToPageIndex(CODE_END);
+
+    if (addr >= SPECIAL_CODE_START && addr <= SPECIAL_CODE_END)
+        return ctrlAddrToPageIndex(SPECIAL_CODE_END);
+
+    return ctrlAddrToPageIndex(VA_END);
+#endif // CTRL_CFG_EXEC_SIZE
 }
 
 Result ctrlReserveExecutablePages(size_t numPages, size_t* outPageIndex) {
     static size_t offset = 0;
+
+#if defined(CTRL_CFG_EXEC_BASE)
+    const size_t pageBase = ctrlAddrToPageIndex(CTRL_CFG_EXEC_BASE);
+#else
     const size_t pageBase = ctrlAddrToPageIndex(ctrlAppSectionInfo()->textAddr);
+#endif // CTRL_CFG_EXEC_BASE
 
     Result ret = findFreeRange(numPages, pageBase + offset, maxPageForCodeBase(pageBase), outPageIndex);
 
